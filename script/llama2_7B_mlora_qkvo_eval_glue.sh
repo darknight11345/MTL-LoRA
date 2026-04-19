b33252662a6955e8f3d4ebca3a8dcf009cf35b8e
darknight11345 <tharani_srinivasan@outlook.com>
2026-04-19 19:18:05 +0200

    Update llama2_7B_mlora_qkvo_eval_glue.sh

--- script/llama2_7B_mlora_qkvo_eval_glue.sh
+++ script/llama2_7B_mlora_qkvo_eval_glue.sh
@@ -12,13 +12,6 @@ SCRIPT_PATH="/pfs/data6/home/ul/ul_student/ul_swv79/MTLLoRA/Code/MTL-LoRA/mlora_
 CHECKPOINT_PATH="/pfs/data6/home/ul/ul_student/ul_swv79/MTLLoRA/Code/MTL-LoRA/output/GLUE/checkpoint/final_checkpoint_old.pt"
 OUTPUT_PATH="/pfs/data6/home/ul/ul_student/ul_swv79/MTLLoRA/Code/MTL-LoRA/output/eval_output/glue"
 
-
-#module load devel/miniforge/24.11.0-python-3.12
-#export PATH="$WS_MODEL/conda/pixtral/bin:$PATH"
-export HF_TOKEN=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
-echo $HF_TOKEN
-
-
 ## 2) CUDA-Modul -------------------------------------------------------
 module load devel/cuda/12.8              # laut `module avail cuda`
 
