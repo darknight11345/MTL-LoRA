84a2c481e36aaa594aba3ec129b01643d1aa7b4b
darknight11345 <tharani_srinivasan@outlook.com>
2026-04-19 19:18:33 +0200

    Update llama2_7B_mlora_qkvo_eval_mmlu.sh

--- script/llama2_7B_mlora_qkvo_eval_mmlu.sh
+++ script/llama2_7B_mlora_qkvo_eval_mmlu.sh
@@ -12,13 +12,6 @@ SCRIPT_PATH="/pfs/data6/home/ul/ul_student/ul_swv79/MTLLoRA/Code/MTL-LoRA/mlora_
 CHECKPOINT_PATH_MMLU="/pfs/data6/home/ul/ul_student/ul_swv79/MTLLoRA/Code/MTL-LoRA/output/MMLU/checkpoint/final_checkpoint.pt"
 OUTPUT_PATH_MMLU="/pfs/data6/home/ul/ul_student/ul_swv79/MTLLoRA/Code/MTL-LoRA/output/eval_output/mmlu"
 
-
-#module load devel/miniforge/24.11.0-python-3.12
-#export PATH="$WS_MODEL/conda/pixtral/bin:$PATH"
-export HF_TOKEN=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
-echo $HF_TOKEN
-
-
 ## 2) CUDA-Modul -------------------------------------------------------
 module load devel/cuda/12.8              # laut `module avail cuda`
 
